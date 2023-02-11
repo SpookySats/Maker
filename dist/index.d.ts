f0d587da3fe3565422e24519a05b67ab790b5c53
SpookySats <118334889+SpookySats@users.noreply.github.com>
2023-02-11 09:58:26 +0200

    spelling mistake
    IDOIT!!!

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export declare const BASE_URL = "https://nft.spookspace.site/";
+export declare const BASE_URL = "https://nft.spookyspace.site/";
 export declare const WIDTH = 20;
 export interface SpotData {
     x: number;
