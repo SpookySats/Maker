882afc952da8ebef7f3f3ac926dd2558f68b4488
SpookySats <118334889+SpookySats@users.noreply.github.com>
2023-02-11 09:34:27 +0200

    added back https

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export declare const BASE_URL = "nft.spookspace.site/";
+export declare const BASE_URL = "https://nft.spookspace.site/";
 export declare const WIDTH = 20;
 export interface SpotData {
     x: number;
